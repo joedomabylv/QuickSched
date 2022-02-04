df18b42b9c6e7489b3bba9f191b4e31abb8454e8
joedomabylv <joedomabylv@gmail.com>
2022-02-03 17:57:08 -0700

    fixtures, routing, emailupload foundation

--- project/quicksched/settings.py
+++ project/quicksched/settings.py
@@ -143,3 +143,7 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = 'sign_in'
+LOGOUT_REDIRECT_URL = 'sign_in'
