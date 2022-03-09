36aa1b2c036936dd73171892f7b8282c91d91575
joedomabylv <joedomabylv@gmail.com>
2022-03-08 18:43:57 -0700

    removed filter bar from LO TA/semester management pages and fixed spinner bug

--- project/static/global/quicksched_javascript/spinners.js
+++ project/static/global/quicksched_javascript/spinners.js
@@ -1,7 +1,6 @@
-var select_button = document.getElementById('possible-spinner')
-
-function replace_with_spinner() {
-    select_button.innerHTML = "<div class=\"spinner-border spinner-border-sm text-dark\" role=\"status\">\
+function replace_with_spinner(element) {
+    element.innerHTML = "<div class=\"spinner-border spinner-border-sm text-dark\" role=\"status\">\
                                    <span class=\"visually-hidden\">Loading...</span>\
                                </div>"
+
 }
