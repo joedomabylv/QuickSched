969a78933e0beff8737e9df0a1eeab708ee895de
Andrew Liddell <aliddell0423@gmail.com>
2022-04-19 17:48:10 -0700

    fixed even more install script issues

--- install.sh
+++ install.sh
@@ -2,18 +2,15 @@
 
 # generate .env file
 django_key=$(echo $RANDOM | md5sum | head -c 30);
-"SECRET_KEY=$django_key" >> env_file;
-
-# rename env file
-mv /home/bitnami/QuickSched/project/env_file /home/bitnami/QuickSched/project/.env
+echo "SECRET_KEY=$django_key" >> env_file;
 
 # rename and enable vhost files
 sudo cp -v /opt/bitnami/apache2/conf/vhosts/sample-vhost.conf.disabled /opt/bitnami/apache2/conf/vhosts/quicksched-vhost.conf;
 sudo cp -v /opt/bitnami/apache2/conf/vhosts/sample-https-vhost.conf.disabled /opt/bitnami/apache2/conf/vhosts/quicksched-https-vhost.conf;
 
 # edit vhost files to configure them for quicksched
-sudo sed -v -r -i 's/sample|SAMPLE/quicksched/g' /opt/bitnami/apache2/conf/vhosts/quicksched-vhost.conf;
-sudo sed -v -r -i 's/sample|SAMPLE/quicksched/g' /opt/bitnami/apache2/conf/vhosts/quicksched-https-vhost.conf;
+sudo sed -r -i 's/sample|SAMPLE/quicksched/g' /opt/bitnami/apache2/conf/vhosts/quicksched-vhost.conf;
+sudo sed -r -i 's/sample|SAMPLE/quicksched/g' /opt/bitnami/apache2/conf/vhosts/quicksched-https-vhost.conf;
 
 # install decouple dependency
 pip -v install python-decouple;
@@ -27,15 +24,21 @@ sudo mkdir -pv /opt/bitnami/projects/quicksched/;
 # deploy django files to specified directory
 sudo mv -v /home/bitnami/QuickSched/project/* /opt/bitnami/projects/quicksched/;
 
+# rename env file
+mv /home/bitnami/QuickSched/project/env_file /home/bitnami/QuickSched/project/.env
+
+# move env file to specified directory
+mv /home/bitnami/QuickSched/project/.env /opt/bitnami/projects/quicksched/
+
 # set up django database
 sudo python /opt/bitnami/projects/quicksched/manage.py makemigrations;
 sudo python /opt/bitnami/projects/quicksched/manage.py migrate;
 
 # create new superuser
-sudo python /opt/bitnami/projects/quicksched/project/manage.py createsuperuser;
+sudo python /opt/bitnami/projects/quicksched/manage.py createsuperuser;
 
 # collect static files for apache to serve
-sudo python /opt/bitnami/projects/quicksched/project/manage.py collectstatic --noinput;
+sudo python /opt/bitnami/projects/quicksched/manage.py collectstatic --noinput;
 
 # change the owner of the django files to the apache wsgi mod daemon
 sudo chown -R daemon:daemon /opt/bitnami/projects/quicksched/
