e185ee97e8d93dd40b4a8ebe23dc84b2d4b819ac
Andrew <you@example.com>
2022-05-02 21:55:41 -0700

    updated email script

--- email_setup.sh
+++ email_setup.sh
@@ -6,5 +6,6 @@ read -r email;
 echo "Enter the app password.";
 read -r password
 
-echo "EMAIL_HOST_USER='$email'" >> /opt/bitnami/projects/quicksched/.env
-echo "EMAIL_HOST_PASSWORD='$password'" >> /opt/bitnami/projects/quicksched/.env
+sudo echo "EMAIL_HOST_USER='$email'" >> /opt/bitnami/projects/quicksched/.env
+sudo echo "EMAIL_HOST_PASSWORD='$password'" >> /opt/bitnami/projects/quicksched/.env
+sudo chown -R daemon:daemon /opt/projects/bitnami/quicksched/*
